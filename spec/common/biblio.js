6256e6a663c0017ee52e50b441ed87ddf03a5bf1
Gregg Kellogg <gregg@greggkellogg.net>
2022-10-17 13:35:36 -0700

    Move localBiblio to a common file.

---
+++ spec/common/biblio.js
@@ -0,0 +1,55 @@
+const rch = {
+  // Add as the respecConfig localBiblio variable
+  // Extend or override global respec references
+  localBiblio: {
+    "CCG-RDC-FINAL": {
+      id: "CCG-RDC-FINAL",
+      title: "RDF Dataset Canonicalization",
+      authors: ["Dave Longley"],
+      editors: [
+        "Dave Longley",
+        "Manu Sporny"
+      ],
+      date: "2022-10-09",
+      href: "https://www.w3.org/community/reports/credentials/CG-FINAL-rdf-dataset-canonicalization-20221009/",
+      status: "CG-FINAL",
+      publisher: "W3C"
+    },
+    "DesignIssues-Diff": {
+      id: "DesignIssues-Diff",
+      title: "Delta: an ontology for the distribution of differences between RDF graphs",
+      authors: ["Tim Berners-Leee"],
+      date: "2015-09-25",
+      href: "https://www.w3.org/DesignIssues/Diff",
+      status: "unofficial",
+      publisher: "W3C"
+    },
+    "eswc2014Kasten": {
+      id: "eswc2014Kasten",
+      title: "A Framework for Iterative Signing of Graph Data on the Web",
+      authors: ["Andreas Kasten", "Ansgar Scherp", "Peter Schauß "],
+      date: "2014",
+      href: "https://doi.org/10.1007/978-3-319-07443-6_11",
+      status: "unofficial",
+      publisher: "ISWC 2014"
+    },
+    "Hogan-Canonical-RDF": {
+      id: "Hogan-Canonical-RDF",
+      title: "Canonical Forms for Isomorphic and Equivalent RDF Graphs: Algorithms for Leaning and Labelling Blank Nodes",
+      authors: ["Aiden Hogan"],
+      date: "2017-07-17",
+      href: "https://aidanhogan.com/docs/rdf-canonicalisation.pdf",
+      status: "unofficial",
+      publisher: "ACM Transactions on the Web"
+    },
+    "HPL-2003-142": {
+      id: "HPL-2003-142",
+      title: "Signing RDF Graphs",
+      authors: ["Jeremy J. Carroll"],
+      date: "2003-07-23",
+      href: "https://www.hpl.hp.com/techreports/2003/HPL-2003-142.pdf",
+      publisher: "HP Laboratories Bristol",
+      status: "unofficial"
+    }
+  }
+};
